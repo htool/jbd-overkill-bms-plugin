18720e96565c117ae2d65be9cb67803fb2749f83
Hans Tool <hans@defect.org>
2023-04-21 21:31:05 +0200

    Default interval to 5 seconds

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = function(app, options) {
       pollFrequency: {
         title: "Poll frequency in seconds",
         type: "number",
-        default: 5000
+        default: 5
 	    },
       MAC: {
         title: "BMS MAC address",
